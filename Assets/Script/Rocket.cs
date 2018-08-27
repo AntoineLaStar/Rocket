9def94c536b329b459f8737e8fdf877d9425662f
Antoine Fortin <tonio349@hotmail.com>
2018-08-27 15:28:22 -0400

    Premier Commit

---
+++ Assets/Script/Rocket.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMovement : MonoBehaviour {
+    public Rigidbody rocketRigidbody;
+    [SerializeField] public float Forceupward = 2000;
+    // Use this for initialization
+    void Start () {
+		rocketRigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            rocketRigidbody.AddForce(new Vector3(0, Forceupward, 0));
+        }
+        else if (Input.GetAxis("Jump") < 0)
+        {
+            rocketRigidbody.AddForce(new Vector3(0, 0, 0));
+        }
+    }
+}
